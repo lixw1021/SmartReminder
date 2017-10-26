17cf3d4b03bbb7f927d53f7a6bf985f18ce6b8f4
xianwei li <xil148@pitt.edu>
2017-10-26 17:53:25 -0400

    implement google place picker

--- app/src/main/java/com/xianwei/smartreminder/fragment/EditLocationFragment.java
+++ app/src/main/java/com/xianwei/smartreminder/fragment/EditLocationFragment.java
@@ -1,13 +1,32 @@
 package com.xianwei.smartreminder.fragment;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.xianwei.smartreminder.EditActivity;
 import com.xianwei.smartreminder.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -15,6 +34,13 @@ import com.xianwei.smartreminder.R;
  */
 public class EditLocationFragment extends Fragment {
 
+    @BindView(R.id.et_location_task)
+    EditText locationTaskEt;
+    @BindView(R.id.tv_location)
+    TextView locationPickerTv;
+
+    private static final String TAG = EditActivity.class.getSimpleName();
+    private static final int PLACE_PICKER_REQUEST = 1;
 
     public EditLocationFragment() {
         // Required empty public constructor
@@ -25,7 +51,40 @@ public class EditLocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_location, container, false);
+        View view =  inflater.inflate(R.layout.fragment_edit_location, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @OnClick(R.id.tv_location)
+    void locationPicker() {
+        try {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            Intent i = builder.build(getActivity());
+            startActivityForResult(i, PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Log.e(TAG, String.format("GooglePlayServices Not Available [%s]", e.getMessage()));
+        } catch (Exception e) {
+            Log.e(TAG, String.format("PlacePicker Exception: %s", e.getMessage()));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK){
+            Place place = PlacePicker.getPlace(getContext(), data);
+            if (place == null) {
+                Toast.makeText(getContext(), "No place picked", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            String placeName = place.getName().toString();
+            String placeAddress = place.getAddress().toString();
+            String placeID = place.getId();
+
+            locationPickerTv.setText(placeName + "\n" + placeAddress + "\n"+ placeID);
+        }
+    }
 }
